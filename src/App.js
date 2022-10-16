8dbdc492ed1ced36bbbef740ececb1a75734ea63
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-10-15 22:27:48 -0500

    reorganized styling sheets

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import './App.css';
 import Banner from './Banner';
 import ImageTitle from './ImageTitle';
 import Message from './Message';
+
+// assign ImageTitle img={aboutUs} or img={howItWorks} to see other ImageTitles
+// assign msg='about' or msg='how-it-works' to see other Messages
 function App() {
   return (
     <div className="App" style={{ justifyContent: "space-between" }}>
         <Banner />
-        <ImageTitle img={keepItSimple} />
-        <Message msg='mission'/>
+        <ImageTitle img={aboutUs} /> 
+        <Message msg='about'/>
     </div>
   );
 }
--- src/Banner.js
+++ src/Banner.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import './Banner.css'
+import './App.css'
 function Banner() {
 
   return (
