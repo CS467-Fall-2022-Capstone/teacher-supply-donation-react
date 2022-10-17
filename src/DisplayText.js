cb4897bb1392cbd3fb3cfec04c4631e42722900d
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-10-16 23:54:03 -0500

    renamed Message as DisplayText and added back to mission buttons

---
+++ src/DisplayText.js
@@ -0,0 +1,86 @@
+import React from 'react'
+
+class DisplayText extends React.Component {
+
+    mission = "Teacher Supply Donation’s mission is to help teachers collect classroom supplies in an efficient, simple way from their local community.";
+
+    onHowBtnClick = () => {
+        this.props.onClick('howItWorks');
+    }
+
+    onMissionBtnClick = () => {
+        this.props.onClick('mission');
+    }
+  
+    colorText(color, text) {
+        return <span style={{ color }}>{text}</span>;
+    }
+
+    displayMsg(whichMsg) {
+        if (whichMsg === 'mission') {
+            return (
+                <div style={{textAlign: 'center', marginLeft: '4em', marginRight: '4em'}}>
+                <h2>
+                    <p>
+                        {this.colorText('red', 'No')} proposals to write. {this.colorText('red', 'No')} budgets to create and manage. {this.colorText('red', 'No')} fuss.
+                    </p>
+                    <p>
+                        {this.mission}
+                    </p>
+                    <p>
+                        <button id="howItWorksBtn" className='ui primary button'  onClick={this.onHowBtnClick} >{this.colorText('white', 'Read how it works.')}</button>
+                    </p>
+                </h2>
+                </div>
+            );
+        
+        } else if (whichMsg === 'howItWorks') {
+            return(
+                <div>
+                    <div style={{textAlign: 'left', marginLeft: '4em', marginRight: '4em'}}>
+                        <h3>
+                            {this.colorText('red', '1.')} Teachers, sign up for a free account then create a list of classroom supplies including the number of each item needed.
+                        </h3>
+                        <h3>
+                            {this.colorText('red', '2.')} Publish this list and receive a unique web link that you can share with students and community members.
+                        </h3>
+                        <h3>
+                            {this.colorText('red', '3.')} Donors can view the list and select the supply item to donate. The supply list is updated automatically and donors receive an automated email notification.
+                        </h3>
+                        <h3>
+                            {this.colorText('red', '4.')} Teachers can view the list of donors and committed supplies, sort the list, and download the list as a .csv file.
+                        </h3>
+                                    
+                    </div>
+                    <button class="ui basic button" onClick={this.onMissionBtnClick} style={{marginTop: '3em', marginBottom: '3em', align: 'center'}}>Back to our mission</button>
+                </div>
+            );
+        } else if (whichMsg === 'aboutUs') {
+            return (
+                <div>
+                    {this.aboutTeam()}
+                    <button class="ui basic button" onClick={this.onMissionBtnClick} style={{marginTop: '3em', marginBottom: '3em'}}>Back to our mission</button>
+                </div>
+            );
+        }
+    }
+
+    aboutTeam() {
+        return (
+            <div className="ui segment" style={{marginLeft: '15rem', marginRight:'15rem'}}>
+                <p style={{textAlign: 'left'}}>Our team...</p>
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div className='ui container'>
+                {this.displayMsg(this.props.msg)}
+            </div>
+        );
+    };
+
+}
+    
+
+export default DisplayText;
