723fa187aa9baf658c2de756a39942b6343ad921
Sean Park <chanseanpark@gmail.com>
2022-11-23 18:18:53 -0800

    restrict delete if donations

--- src/components/TeacherDashboard/DeleteModal.js
+++ src/components/TeacherDashboard/DeleteModal.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Header, Icon, Modal, Popup } from 'semantic-ui-react';
+import { Button, Header, Icon, Modal } from 'semantic-ui-react';
 import { FaTrashAlt } from 'react-icons/fa';
 
 function DeleteModal({ supply, onDelete }) {
