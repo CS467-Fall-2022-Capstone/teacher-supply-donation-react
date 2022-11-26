23b179d32c51a006f6d254f8d4d49a1f5d14d3b5
fisher-alice <107423305+fisher-alice@users.noreply.github.com>
2022-11-26 17:37:56 -0600

    add joels blurb to about us page

--- src/components/DisplayText.js
+++ src/components/DisplayText.js
@@ -45,7 +45,12 @@ class DisplayText extends React.Component {
         },
         {
             title: 'Joel Swendaal',
-            content: `Joe Swendaal is a...`
+            content: `Joe Swendaal has a Master’s degree in English Language Education 
+            from San Francisco State University and worked in student success roles in higher education 
+            for a decade before returning to study toward a BS in Computer Science at Oregon State. 
+            He is especially interested in developing applications with an educational component, as well 
+            as implementing systems that use data to improve student success outcomes. 
+            Following graduation, he will be working as a Data Analyst at the University of Illinois.`
         }
     ];
 
